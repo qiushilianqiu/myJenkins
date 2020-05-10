e676faac5cc381e218776f132610879a75b06c26
qsl011 <757724456@qq.com>
2020-05-10 18:54:19 +0800

    第一次提交

---
+++ src/main/java/com/jenkins/demo/controller.java
@@ -0,0 +1,13 @@
+package com.jenkins.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @RequestMapping("/test")
+    public String testJenKins() {
+        return "欢迎测试jenkins！！！";
+    }
+}
